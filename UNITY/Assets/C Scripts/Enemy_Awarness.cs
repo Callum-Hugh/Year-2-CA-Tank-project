686dfd26ac04550da6596e00f7c6ef1eeeb4f62a
Callum-Hugh <D00279407@student.dkit.ie>
2025-11-07 00:35:44 +0000

    Start to make the fix enemy code to follow the player when close

---
+++ UNITY/Assets/C Scripts/Enemy_Awarness.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Awarness : MonoBehaviour
+{
+
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float awaerenessDistance;
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 enemyPositionToPlayer = player.position - transform.position;
+        DirectionToPlayer = enemyPositionToPlayer.normalized;
+
+        if (enemyPositionToPlayer.magnitude <= awaerenessDistance)
+        {
+            AwareOfPlayer = true;
+        }
+        else
+        {
+            AwareOfPlayer = false;  
+        }
+    }
+}
--- UNITY/Assets/C Scripts/Enemy_Tank.cs
+++ UNITY/Assets/C Scripts/Enemy_Tank.cs
@@ -1,4 +1,3 @@
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Enemy_Tank : MonoBehaviour
@@ -18,61 +17,120 @@ public class Enemy_Tank : MonoBehaviour
     private bool isIdle = false;
     private float idleTime = 1;
 
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Rigidbody2D rb;
+
+    private Enemy_Awarness ea;
+    private Enemy_Awarness enemyAwarness;
+
+    private Vector2 targetDirection;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        //_animator = GetComponent<Animator>();
-        timeInDirection = distanceTime;
-    }
+    //void Start()
+    //{
+    //_animator = GetComponent<Animator>();
+    //    timeInDirection = distanceTime;
+    //}
 
     // Update is called once per frame
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        enemyAwarness = GetComponent<Enemy_Awarness>();
+    }
+
     void Update()
     {
-        if (!isDead)
+        //  if (!isDead)
+        //  {
+
+        //      if (isIdle && idleTime < 0)
+        //      {
+        //        directionX = directionX * -1;
+        //       timeInDirection = distanceTime;
+        //       isIdle = false;
+        //_animator.SetInteger("Direction X", directionX);
+        //_animator.SetInteger("Direction Y", directionY);
+        //_animator.SetFloat("Move", 1);
+        // }
+
+        //else if (!isIdle && timeInDirection < 0)
+        //{
+        //  isIdle = true;
+        // idleTime = 1;
+        //}
+
+        //if (!isIdle)
+        //{
+        //    Vector2 pos = transform.position;
+        //    pos.x = pos.x + (speed * Time.deltaTime * directionX);
+        //    transform.position = pos;
+        //    timeInDirection -= Time.deltaTime;
+        //}
+
+        //if (!isIdle)
+        //{
+        //    Vector2 pos = transform.position;
+        //    pos.y = pos.y + (speed * Time.deltaTime * directionY);
+        //    transform.position = pos;
+        //    timeInDirection -= Time.deltaTime;
+        //}
+
+
+        //        else
+        //        {
+        //            idleTime -= Time.deltaTime;
+        //        }
+
+        //    }
+    }
+
+    void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTwordsTarget();
+        SetVolocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (Enemy_Awarness.AwareOfPlayer)
         {
-
-            if (isIdle && idleTime < 0)
-            {
-                directionX = directionX * -1;
-                timeInDirection = distanceTime;
-                isIdle = false;
-                //_animator.SetInteger("Direction X", directionX);
-                //_animator.SetInteger("Direction Y", directionY);
-                //_animator.SetFloat("Move", 1);
-            }
-
-            else if (!isIdle && timeInDirection < 0)
-            {
-                isIdle = true;
-                idleTime = 1;
-            }
-
-            if (!isIdle)
-            {
-                Vector2 pos = transform.position;
-                pos.x = pos.x + (speed * Time.deltaTime * directionX);
-                transform.position = pos;
-                timeInDirection -= Time.deltaTime;
-            }
-
-            //if (!isIdle)
-            //{
-            //    Vector2 pos = transform.position;
-            //    pos.y = pos.y + (speed * Time.deltaTime * directionY);
-            //    transform.position = pos;
-            //    timeInDirection -= Time.deltaTime;
-            //}
-
-
-            else
-            {
-                idleTime -= Time.deltaTime;
-            }
-
+            targetDirection = Enemy_Awarness.DirectionToPlayer;
         }
 
-
-
-
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
     }
+
+    private void RotateTwordsTarget()
+    {
+        if (targetDirection != Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transfrom.forword, targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        rb.SetRotation(rotation);
+    }
+
+    private void SetVolocity()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+           rb.velocity = transform.up * speed
+        }
+    }    
+
 }
